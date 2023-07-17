5e7c29015798392d2c26cecb0c3fdb3664e2c2df
Bin <1341609868@qq.com>
2023-07-18 02:10:15 +0800

    优化

---
+++ app/common/model/RechargeOrderModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\common\model;
+
+/**
+ * 轮播图
+ * @author Bin
+ * @time 2023/7/6
+ */
+class RechargeOrderModel extends  BaseModel
+{
+    protected $name = 'recharge_order';
+}
