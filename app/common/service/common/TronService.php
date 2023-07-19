ca62332edf0ff174caa6eb887a412adbd15c51b4
service <1@qq.com>
2023-07-19 21:20:13 +0800

    优化

--- app/common/service/common/TronService.php
+++ app/common/service/common/TronService.php
@@ -23,7 +23,7 @@ class TronService
         $this->host = env('tron_host', 'https://api.trongrid.io');
         $this->tron_scan = env('tron_scan', 'https://api.tronscan.org');
         $header = [
-            'TRON_PRO_API_KEY: a84021ad-2f2c-4154-bb07-259b3f16feed'
+            'TRON_PRO_API_KEY' => 'a84021ad-2f2c-4154-bb07-259b3f16feed'
         ];
         $http_provider = new HttpProvider($this->host, 10000, false, false, $header);
         // 创建一个tron对象
@@ -166,14 +166,14 @@ class TronService
     {
         $url = $this->tron_scan . '/api/account/wallet?address=' . $address;
         try {
-            $config = [
-                'handler' => 'TRON-PRO-API-KEY:aacc3f55-4566-435b-b445-dfa667b2829f'
+            $headers = [
+                'TRON-PRO-API-KEY' => 'aacc3f55-4566-435b-b445-dfa667b2829f'
             ];
-            $client = new Client($config);
-            $result = $client->get($url);
-            if ($result == '') {
-                return $this->wallet($address);
-            }
+            $client = new Client();
+            $response = $client->get($url, $headers);
+            // 获取响应内容
+            $result = $response->getBody()->getContents();
+            
             return json_decode($result, true);
         } catch (\Exception $e) {
             echo "ERROR:" . $e->getLine() . ":" . $e->getMessage() . "\n";
