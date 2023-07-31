eb87a570550812eea02508ccaf44c7be70e634ee
ttff3306 <ttff3306@gmail.com>
2023-07-31 19:04:48 +0800

    优化

--- public/assets/js/backend/wallet/token_balance.js
+++ public/assets/js/backend/wallet/token_balance.js
@@ -27,11 +27,14 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'id', title: __('Id')},
                         {field: 'address', title: __('Address')},
                         {field: 'chain', title: __('Chain')},
-                        {field: 'balance', title: __('Balance'), operate:'BETWEEN'},
+                        {field: 'balance', title: __('Balance'), operate:'BETWEEN', sortable: true},
                         {field: 'token', title: __('Token')},
+                        {field: 'total_token_value', title: __('折合公链原生代币数量'), sortable: true},
+                        {field: 'value_usd', title: __('折合USD数量'), sortable: true},
+                        {field: 'token_contract_address', title: __('代币合约地址')},
                         {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
                         {field: 'update_time', title: __('Update_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                        // {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
             });
