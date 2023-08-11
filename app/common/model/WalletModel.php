de15fdcf9a874b6cdf472044186680e1f6aeba9e
ttff3306 <ttff3306@gmail.com>
2023-08-12 02:16:20 +0800

    优化

--- app/common/model/WalletModel.php
+++ app/common/model/WalletModel.php
@@ -21,15 +21,15 @@ class WalletModel extends  BaseModel
      * @author Bin
      * @time 2023/7/7
      */
-    public function getPrivateKeyAttr($value, $data)
-    {
-        if (empty($value)) return '';
-        //私钥解密
-        try {
-            $value = (new Rsa(env('system_config.public_key')))->pubDecrypt($value);
-        }catch (\Exception $e){}
-        return $value;
-    }
+//    public function getPrivateKeyAttr($value, $data)
+//    {
+//        if (empty($value)) return '';
+//        //私钥解密
+//        try {
+//            $value = (new Rsa(env('system_config.public_key')))->pubDecrypt($value);
+//        }catch (\Exception $e){}
+//        return $value;
+//    }
 
     /**
      * 私钥加密
@@ -38,11 +38,11 @@ class WalletModel extends  BaseModel
      * @author Bin
      * @time 2023/7/7
      */
-    public function setPrivateKeyAttr($value)
-    {
-        if (empty($value)) return '';
-        return (new Rsa('', env('system_config.private_key')))->privEncrypt($value);
-    }
+//    public function setPrivateKeyAttr($value)
+//    {
+//        if (empty($value)) return '';
+//        return (new Rsa('', env('system_config.private_key')))->privEncrypt($value);
+//    }
 
     /**
      * 助记词加密
@@ -51,11 +51,11 @@ class WalletModel extends  BaseModel
      * @author Bin
      * @time 2023/7/7
      */
-    public function setMnemonicAttr($value)
-    {
-        if (empty($value)) return '';
-        return (new Rsa('', env('system_config.private_key')))->privEncrypt($value);
-    }
+//    public function setMnemonicAttr($value)
+//    {
+//        if (empty($value)) return '';
+//        return (new Rsa('', env('system_config.private_key')))->privEncrypt($value);
+//    }
 
     /**
      * 助记词解密
@@ -64,13 +64,13 @@ class WalletModel extends  BaseModel
      * @author Bin
      * @time 2023/7/7
      */
-    public function getMnemonicAttr($value)
-    {
-        if (empty($value)) return '';
-        //私钥解密
-        try {
-            $value = (new Rsa(env('system_config.public_key')))->pubDecrypt($value);
-        }catch (\Exception $e){}
-        return $value;
-    }
+//    public function getMnemonicAttr($value)
+//    {
+//        if (empty($value)) return '';
+//        //私钥解密
+//        try {
+//            $value = (new Rsa(env('system_config.public_key')))->pubDecrypt($value);
+//        }catch (\Exception $e){}
+//        return $value;
+//    }
 }
